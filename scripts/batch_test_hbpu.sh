94f6889182c95bf7fe206a79b06bf79139612369
SheepHuan <justno@foxmail.com>
2024-11-13 14:25:20 +0800

    1

--- scripts/batch_test_hbpu.sh
+++ scripts/batch_test_hbpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 指定要搜索的目录
-search_dir="/home/sunrise/DeployNPUs/saves/onnx-10"
+search_dir="/home/sunrise/DeployNPUs/saves/onnx3-10"
 benchmark_path="/home/sunrise/DeployNPUs/build/hbpu_test"
 # 使用find命令查找所有.bin文件，并通过xargs传递给循环
 find "$search_dir" -type f -name "*.bin" | while read -r file_path; do
